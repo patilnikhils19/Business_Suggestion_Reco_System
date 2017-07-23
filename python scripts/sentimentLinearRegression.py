6b1170042b9e8527393c4eb98a5c8ba5640fdb4a
Nikhil Patil <nspatil@denver.cs.colostate.edu>
2017-07-23 12:59:19 -0600

    Corrcted comments

--- python scripts/sentimentLinearRegression.py
+++ python scripts/sentimentLinearRegression.py
@@ -1,11 +1,7 @@
 print(__doc__)
 
-##!/usr/bin/python
-##!/usr/bin/env python
 
-
-# Code source: Jaques Grobler
-# License: BSD 3 clause
+#Author Nikhil Patil patilnikhils19@gmail.com
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -59,24 +55,21 @@ np.save("X_total_senti", X_total)
 X_total = np.load('X_total_senti.npy')
 print "X_total + labels 2", X_total.shape, X_total[0], X_total[-1]
 
-# # Load the diabetes dataset
-# diabetes = datasets.load_diabetes()
-# print("diabetes data shape", diabetes.data.shape)
 
 # Use only one feature
-diabetes_X = X_total[:, np.newaxis, 0]  # diabetes.data[:, np.newaxis, 2]
-print("diabetes X data shape new", diabetes_X.shape, diabetes_X.dtype, diabetes_X[0], diabetes_X[-3])
-print("diabetes y data shape new", y_total[0], y_total[-3])
+data_X = X_total[:, np.newaxis, 0]  # diabetes.data[:, np.newaxis, 2]
+print("X data shape new", data_X.shape, data_X.dtype, data_X[0], data_X[-3])
+print("y data shape new", y_total[0], y_total[-3])
 
 # Split the data into training/testing sets
-diabetes_X_train = diabetes_X[:-split_index_test]
-diabetes_X_test = diabetes_X[-split_index_test:]
-print("data train test X data shape", diabetes_X_train.shape, diabetes_X_test.shape)
+data_X_train = data_X[:-split_index_test]
+data_X_test = data_X[-split_index_test:]
+print("data train test X data shape", data_X_train.shape, data_X_test.shape)
 
 # Split the targets into training/testing sets
-diabetes_y_train = y_total[:-split_index_test] # diabetes.target[:-split_index_test]
-diabetes_y_test =  y_total[-split_index_test:] # diabetes.target[-split_index_test:]
-print("data train test y data shape", diabetes_y_train.shape, diabetes_y_test.shape)
+data_y_train = y_total[:-split_index_test] 
+data_y_test =  y_total[-split_index_test:] 
+print("data train test y data shape", data_y_train.shape, data_y_test.shape)
 
 
 
@@ -84,18 +77,18 @@ print("data train test y data shape", diabetes_y_train.shape, diabetes_y_test.sh
 regr = linear_model.LinearRegression()
 
 # Train the model using the training sets
-regr.fit(diabetes_X_train, diabetes_y_train)
+regr.fit(data_X_train, data_y_train)
 
 # The coefficients
 print('Coefficients: \n', regr.coef_)
 # The mean squared error
-print("Mean squared error: %.2f" % np.mean(((regr.predict(diabetes_X_test)) - diabetes_y_test) ** 2))
+print("Mean squared error: %.2f" % np.mean(((regr.predict(data_X_test)) - data_y_test) ** 2))
 # Explained variance score: 1 is perfect prediction
-print('Variance score: %.2f' % regr.score(diabetes_X_test, diabetes_y_test))
+print('Variance score: %.2f' % regr.score(data_X_test, data_y_test))
 
 # Plot outputs
-plt.scatter(diabetes_X_test, diabetes_y_test, alpha=0.8, marker='.', color='black')
-plt.plot(diabetes_X_test, regr.predict(diabetes_X_test), color='blue',
+plt.scatter(data_X_test, data_y_test, alpha=0.8, marker='.', color='black')
+plt.plot(data_X_test, regr.predict(data_X_test), color='blue',
          linewidth=3)
 
 plt.xlabel('average sentiment value per business', fontsize=20)
